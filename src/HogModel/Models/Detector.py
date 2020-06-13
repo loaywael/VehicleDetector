17f46366f2d4e06f71a070fc279c9acfbac8e3bf
loaywael <hythamconn1@gmail.com>
2020-06-13 19:44:02 +0200

    added class docs

--- src/HogModel/Models/Detector.py
+++ src/HogModel/Models/Detector.py
@@ -8,15 +8,72 @@ import cv2
 
 
 class Detector:
+    """
+    HogDetector using sliding window and image pyramids
 
-    def __init__(self, windowSize, steps=2, conf_thresh=0.55):
+    Attributes
+    ----------
+    nmsThreshold : float
+        non max threshold to ignore proposals with low confidence score
+    classifier : Classifier object
+        linear classifier to classify scaned windows
+    w : int
+        sliding window width
+    h : int
+        sliding window height
+    steps: int
+        stride of the sliding window
+
+    Methods
+    -------
+    _get_roi_patches(X, visualize=False, delay=10, limits=None)
+        applies sliding window for a given scale
+    _get_height_limits(X)
+        limits the sliding window to certain height from : to
+    _drawBBoxes(X, bBoxes)
+        draws bounding boxes and other infro {score, etc} to img
+    _get_heat_map(self, img, bboxes, threshold, visualize=False)
+        threshold multiple proposals to remove false positives
+    detectMultiScale(self, X, levels=3, threshold=5, visualize=False, height_limits=None)
+        detect objects at multi-scales using image pyramids
+    """
+    def __init__(self, windowSize, steps=16, conf_thresh=0.55):
+        """
+        Parameters
+        ----------
+        windowSize : tuple
+            sliding window (width, height)
+        steps : int
+            sliding window stride (default is 16)
+        conf_thresh : float
+            non max proposal thresholding (default is 0.55)
+        """
         self.nmsThreshold = conf_thresh
         self.classifier = Classifier()
         self.w, self.h = windowSize
         self.steps = steps
 
     # @profiling.timer
-    def _getROIBoxes(self, X, visualize=False, fps=10, limits=None):
+    def _get_roi_patches(self, X, visualize=False, delay=5, limits=None):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            img scale to scane with sliding window
+        visualize : bool
+            visualizing the scan process (default is False)
+        delay : int
+            controls the visualization scan speed (default is 5) 
+        limits : tuple
+            limits the sliding window scanning height (default is None)
+        
+        Returns
+        -------
+        patches : np.ndarray
+            ROI patches >> (-1, 64, 64, -1)
+        bBoxes : np.ndarray
+            Bounding Boxes locations of all windows >> (-1, x, y, w, h)
+        """
         x, y = 0, 0
         if limits:
             yStart, yEnd = limits
@@ -27,6 +84,7 @@ class Detector:
         steps_per_height = int((X.shape[0] - self.h) / self.steps) + 1
         patches = []
         bBoxes = []
+
         for yi in range(1, steps_per_height+1):
             if yStart and y+self.h >= yEnd:
                 break
@@ -37,42 +95,87 @@ class Detector:
                     clone = X.copy()
                     cv2.rectangle(clone, (x, y), (x+self.w, y+self.h), (0, 255, 0), 3)
                     cv2.imshow("clone", clone)
-                    cv2.waitKey(fps)
+                    cv2.waitKey(delay)
                 patches.append(patch)
                 bBoxes.append(bBox)
                 x += self.steps
             x = 0
             y += self.steps
+            
         patches = np.array(patches)
         bBoxes = np.array(bBoxes)
         return patches, bBoxes
 
     @staticmethod
-    def get_height_limits(X):
+    def _get_height_limits(X):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            img scale to scane with sliding window
+       
+        Returns
+        -------
+        limits : tuple
+            height limits for the given image scale
+        """
         bottom_half_start = int(0.5 * X.shape[0])
         bottom_half_end = int(0.9 * X.shape[0])
-        return bottom_half_start, bottom_half_end
+        limits = bottom_half_start, bottom_half_end
+        return limits
 
     @staticmethod
     # @profiling.timer
-    def drawBBoxes(X, bBoxes):
+    def _drawBBoxes(X, bBoxes):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            output image with original shape
+        bBoxes : np.ndarray
+            propasal boxes locations >> (-1, x, y, w, h, p)
+
+        Returns
+        -------
+        X : np.ndarray
+            detected image with drawings of the proposal boxes
+        """
         img = X.copy()
         if len(bBoxes) > 0:
             for x, y, w, h, p in bBoxes:
                 x, y, w, h = int(x), int(y), int(w), int(h)
                 cv2.rectangle(img, (x, y), (x+32, y+16), (0, 255, 0), -1)
                 cv2.putText(img, f"{p:.2f}", (x + 2, y+10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
                 cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         return img
 
-    def get_heat_map(self, img, bboxes, threshold, visualize=False):
+    def _get_heat_map(self, img, bboxes, threshold, visualize=False):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            output image with original shape
+        bboxes : np.ndarray
+            filtered propasal boxes locations >> (-1, x, y, w, h, p)
+        threshold : int
+            unify proposal that hass above or equal occurences than threshold
+            and removes others below it
+        visualize : bool
+            visualizing heatmap grouping result
+
+        Returns
+        -------
+        X : np.ndarray
+            detected image with drawings of the proposal boxes
+        """
         heatmap = np.zeros_like(img[:, :, 0]).astype(np.float)
         for box in bboxes:
             x, y, w, h = box[:4].astype("int")
             heatmap[y:y+h, x:x+w] += 1
         heatmap[heatmap <= threshold] = 0
         locations, ncars = label(heatmap)
+
         for i in range(1, ncars+1):
             nonzero_vals = (locations == i).nonzero()
             nonzeroY = np.array(nonzero_vals[0])
@@ -81,21 +184,45 @@ class Detector:
             bottom_right = (np.max(nonzeroX), np.max(nonzeroY))
             cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
         print("detected cars in frame: ", ncars)
+
         if visualize:
             plt.imshow(heatmap)
             plt.show()
         return img, ncars
 
     @profiling.timer
-    def detectMultiScale(self, X, levels=3, threshold=5, 
-        scale=0.50, visualize=False, height_limits=None):
+    def detectMultiScale(self, X, levels=6, threshold=5, 
+        visualize=True, height_limits=None):
         """
+        Parameters
+        ----------
+        X : np.ndarray
+            img src to detect object in
+        levels : int
+            max number of pyramid levels to scan (default is 6)
+        threshold : int
+            unify proposal that hass above or equal occurences than threshold
+            and removes others below it (default is 5)
+        visualize : bool
+            visualizing the detection as image or return proposal boxes (default is True)
+        height_limits : bool
+            limits the sliding window scanning height (default is None)
+        
+        Returns
+        -------
+        X : np.ndarray
+            detected image with drawings of the proposal boxes
+        or
+        bBoxes : np.ndarray
+            filtered proposal boxes >> (-1, x, y, w, h, p)
         """
+        scale = 0.50
         steps = self.steps
         downSampled = X.copy()
         limits = None
         all_bBoxes = []
         all_windows = []
+
         for level in range(1, levels+1):
             if level == 1:
                 upscale_factor = 1
@@ -106,8 +233,8 @@ class Detector:
                 # downSampled = cv2.GaussianBlur(downSampled, (3, 3), 0)
                 upscale_factor = (1/scale) * (level - 1)
             if height_limits:
-                limits = Detector.get_height_limits(downSampled)
-            windows, bBoxes = self._getROIBoxes(downSampled, limits=limits, visualize=False)
+                limits = Detector._get_height_limits(downSampled)
+            windows, bBoxes = self._get_roi_patches(downSampled, limits=limits, visualize=False)
             if len(windows) < 1:
                 break
             print("level: ", level, "sride: ", self.steps)
@@ -118,14 +245,16 @@ class Detector:
                 bBoxes = bBoxes * upscale_factor
                 all_bBoxes.append(bBoxes)
                 all_windows.append(windows)
+
         self.steps = steps
         all_bBoxes = np.concatenate(all_bBoxes)
         all_windows = np.concatenate(all_windows)
         predictions = self.classifier(all_windows)
         all_bBoxes = np.hstack([all_bBoxes, predictions[:, 1].reshape(-1, 1)])
+
         filteredBoxes = all_bBoxes[all_bBoxes[:, -1] > self.nmsThreshold]
-        detectedImg, _ = self.get_heat_map(X, filteredBoxes, threshold, visualize=True)
+        detectedImg, _ = self._get_heat_map(X, filteredBoxes, threshold, visualize=True)
         if visualize:
-            # detectedImg = self.drawBBoxes(X, filteredBoxes)
+            # detectedImg = self._drawBBoxes(X, filteredBoxes)
             return detectedImg
         return filteredBoxes
--- src/HogModel/Models/Preprocessor.py
+++ src/HogModel/Models/Preprocessor.py
@@ -8,37 +8,90 @@ import cv2
 
 class FeatureDescriptor(BaseEstimator, TransformerMixin):
     """
+    Custom Preprocessor Extracts Features Vector from Batch of Images
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    fit(self, X, y=None)
+    transform(self, X, y=None)
+    _preprocess_img(X)
+    _get_color_hist_features(img)
+    _get_color_features(img, downSize=(32, 32))
+    _get_hog_features(img, win_size=(64, 64), block_size=(16, 16),
+        block_stride=(8, 8), cell_size=(8, 8), nbins=9)
     """
     def __init__(self):
+        """
+        """
         pass
 
     def fit(self, X, y=None):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            batch of images to fit preprocessor over
+        y : np.ndarray
+            batch of labels associated with images batch
+
+        Returns
+        -------
+        self : FeatureDescriptor object
+            fitted preprocessor object
+        """
         self.scaler = preprocessing.StandardScaler()
         return self 
 
     @profiling.timer
     def transform(self, X, y=None):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            batch of images to extract features from
+        y : np.ndarray
+            batch of labels associated with images batch
+
+        Returns
+        -------
+        features : np.ndarray
+            extracted features vector
+        """
         if (len(X.shape) == 3) and (X.shape[2] <= 3):
-            return self._preprocessImg(X)
+            return self._preprocess_img(X)
         else:
             try:    # will be threaded later
-                return np.concatenate([self._preprocessImg(x) for x in X], axis=0)
+                return np.concatenate([self._preprocess_img(x) for x in X], axis=0)
             except Exception as e:
                 print("ERROR MSG: ", e)
                 print("Incorrect image shape should be (64, 64, 3), or (-1, 64, 64, 3)")
 
     @staticmethod
     # @profiling.timer
-    def _preprocessImg(X):
+    def _preprocess_img(X):
+        """
+        Parameters
+        ----------
+        X : np.ndarray
+            rgb image to extract features vector from 
+
+        Returns
+        -------
+        featuresVector : np.ndarray
+            extracted features vector
+        """
         hls = cv2.cvtColor(X, cv2.COLOR_RGB2HLS)
         ycbcr = cv2.cvtColor(X, cv2.COLOR_RGB2YCrCb)
         gray = cv2.cvtColor(X, cv2.COLOR_RGB2GRAY)
         edges = cv2.Canny(gray, 33, 75, L2gradient=True)
 
-        edgesFeatures = FeatureDescriptor._getColorFeatures(edges)
-        hlsHistFeatures = FeatureDescriptor._getColorHistFeatures(hls)
-        ycbcrColorFeatures = FeatureDescriptor._getColorFeatures(ycbcr)
-        hogFeatures = FeatureDescriptor._getHogFeatures(gray)
+        edgesFeatures = FeatureDescriptor._get_color_features(edges)
+        hlsHistFeatures = FeatureDescriptor._get_color_hist_features(hls)
+        ycbcrColorFeatures = FeatureDescriptor._get_color_features(ycbcr)
+        hogFeatures = FeatureDescriptor._get_hog_features(gray)
 
         featuresVector = np.concatenate([
         hogFeatures,
@@ -50,21 +103,48 @@ class FeatureDescriptor(BaseEstimator, TransformerMixin):
 
 
     @staticmethod
-    def _getHogFeatures(img, win_size=(64, 64), block_size=(16, 16),
+    def _get_hog_features(img, win_size=(64, 64), block_size=(16, 16),
             block_stride=(8, 8), cell_size=(8, 8), nbins=9):
+        """
+        img : np.ndarray
+            gray-scale image to extract hog features vector from 
+
+        Returns
+        -------
+        hogFeatures : np.ndarray
+            extracted hog features vector
+        """
         hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
         hogFeatures = hog.compute(img).squeeze()
         return hogFeatures
 
     @staticmethod
     # @profiling.timeit
-    def _getColorFeatures(img, downSize=(32, 32)):
+    def _get_color_features(img, downSize=(32, 32)):
+        """
+        img : np.ndarray
+            rgb image to extract color features vector from 
+
+        Returns
+        -------
+        colorFeatures : np.ndarray
+            extracted color features vector
+        """
         colorFeatures = cv2.resize(img, downSize).ravel()
         return colorFeatures
 
     @staticmethod
     # @profiling.timer
-    def _getColorHistFeatures(img):
+    def _get_color_hist_features(img):
+        """
+        img : np.ndarray
+            rgb image to extract color histogram features vector from 
+
+        Returns
+        -------
+        featuresHistogram : np.ndarray
+            extracted color histogram features vector
+        """
         hists = [cv2.calcHist([img], [i], None, [32], [0, 256]) for i in range(3)]
         featuresHistogram = np.array(hists).ravel()
         return featuresHistogram
